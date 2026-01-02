fa8be52764c5ad2807538263bc1b909aec4fbc9f
ctc96 <sparsh@concepttcode.in>
2026-01-02 16:51:09 +0530

    update

--- wp_sync_poster.py
+++ wp_sync_poster.py
@@ -160,7 +160,7 @@ def _build_post_body(pl: ParsedListing) -> Dict[str, Any]:
 
     # # static
     # body.setdefault("deal_type", ["MLS Deals"])
-    body.setdefault("newest_deals", ["Today's Deal"])
+    body.setdefault("newest_deals", ["Todays Deal"])
 
     return body
 
